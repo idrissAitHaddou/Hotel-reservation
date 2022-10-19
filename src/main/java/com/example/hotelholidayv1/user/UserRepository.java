21cc74b935aff47d307d29d85627be47f6d6757e
idriss <idrissaithadou@gmail.com>
2022-10-19 13:37:33 +0100

    added some functions in repo admin

--- src/main/java/com/example/hotelholidayv1/user/UserRepository.java
+++ src/main/java/com/example/hotelholidayv1/user/UserRepository.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserRepository extends Connection {
-    // get all users
+    // * get all users
     ResultSet GET() {
         ResultSet user = null;
         String query = "select * from admins";
