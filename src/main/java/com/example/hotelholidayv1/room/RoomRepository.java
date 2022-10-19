06933af796a25d138d29c7ddbeb0f7d4e6f291f5
EL Ayachi Abdelmajid <93975585+EL-Ayachi-Abdelmajid@users.noreply.github.com>
2022-10-19 09:38:57 +0100

    add : database

--- src/main/java/com/example/hotelholidayv1/room/RoomRepository.java
+++ src/main/java/com/example/hotelholidayv1/room/RoomRepository.java
@@ -1,4 +1,26 @@
 package com.example.hotelholidayv1.room;
 
-public class RoomRepository {
+import com.example.hotelholidayv1.DB.Connection;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class RoomRepository extends Connection {
+
+//    public static <T> List<T> getAllRooms(){
+////        List<List<HashMap<String,String>>> list = new ArrayList<>();
+//        try {
+//            StringBuilder sql = new StringBuilder("SELECT * FROM rooms;");
+//            ResultSet resultSet = connect().executeQuery(String.valueOf(sql));
+//            while (resultSet.next()){
+//
+//            }
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return list;
+//    }
 }
