181ac238e9da6f95ec4c68be6d93008038d661dd
EL Ayachi Abdelmajid <93975585+EL-Ayachi-Abdelmajid@users.noreply.github.com>
2022-10-20 14:28:08 +0100

    add : config to admin create : data converter

--- src/main/java/com/example/hotelholidayv1/helpers/DataConverter.java
+++ src/main/java/com/example/hotelholidayv1/helpers/DataConverter.java
@@ -1,15 +1,38 @@
 package com.example.hotelholidayv1.helpers;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import static jdk.nashorn.api.scripting.ScriptUtils.convert;
 
 public class DataConverter {
 
-    public <T> ArrayList<T> fromRsToArray(ResultSet rsData){
-        ArrayList<T> data = new ArrayList<>();
+    public static <T> ArrayList<Object> fromRsToArray(ResultSet rsData, String className) throws ClassNotFoundException, SQLException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        ArrayList<Object> dataArray = new ArrayList<>();
+        Class<?> targetClass = Class.forName(className);
+        for (Constructor<?> ctor : targetClass.getConstructors()) {
+            Class<?>[] paramTypes = ctor.getParameterTypes();
 
-      return data;
+            // If the arity matches, let's use it.
+//            int numberOfColumn = (int) rsData.getColumnCount();
+//            if (paramTypes.length == numberOfColumn) {
+
+                // Convert the String arguments into the parameters' types.
+                Object[] convertedArgs = new Object[paramTypes.length];
+                while (rsData.next()){
+                    for (int i = 0; i < convertedArgs.length; i++) {
+                        convertedArgs[i] = convert(paramTypes[i], rsData.getString(paramTypes[i].getModifiers()));
+                    }
+                // Instantiate the object with the converted arguments.
+                    dataArray.add(ctor.newInstance(convertedArgs));
+                }
+
+            }
+
+      return dataArray;
     }
 
 //    public Object instantiate(List<String> args,String className) throws ClassNotFoundException {
@@ -18,5 +41,7 @@ public class DataConverter {
 //            Class<?>[] paramTypes = ctor.getParameterTypes();
 //
 //    }
+
+
 
 }
