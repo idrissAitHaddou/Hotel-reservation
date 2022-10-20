493fdb7d6525d024bc74bca6db03cecf162def3a
idriss <idrissaithadou@gmail.com>
2022-10-20 16:35:13 +0100

    feat

--- src/main/java/com/example/hotelholidayv1/extras/ExtrasRepository.java
+++ src/main/java/com/example/hotelholidayv1/extras/ExtrasRepository.java
@@ -1,37 +1,4 @@
 package com.example.hotelholidayv1.extras;
 
-import com.example.hotelholidayv1.DAO.DAOManager;
-
-import java.sql.ResultSet;
-
-public class ExtrasRepository extends DAOManager {
-    static ResultSet all(int id){
-        StringBuilder query = new StringBuilder("SELECT * FROM extras ");
-        if(id != 0){
-            query.append(" WHERE id_extra = ").append(id);
-        }
-        query.append(";");
-        return get(query);
-    }
-    static boolean save(Extras extra){
-        StringBuilder query = new StringBuilder("INSERT INTO extras ( rate, type_extra ) values(");
-        query.append("'").append(extra.rate).append("'");
-        query.append(",").append("'").append(extra.type_extra).append("'");
-        query.append(");");
-        return post(query);
-    }
-    static boolean update(Extras extra,int id){
-        StringBuilder query = new StringBuilder("UPDATE extras SET");
-        query.append(" rate = ").append("'").append(extra.rate).append("'");
-        query.append(" type_extra = ").append("'").append(extra.type_extra).append("'");
-        query.append(" WHERE id_extra = ").append(id);
-        return post(query);
-    }
-
-    static boolean delete(int id){
-        StringBuilder query = new StringBuilder("DELETE FROM extras WHERE id_extra = ");
-        query.append(id);
-        query.append(";");
-        return post(query);
-    }
+public class ExtrasRepository {
 }
--- src/main/java/com/example/hotelholidayv1/user/UserAdminServlet.java
+++ src/main/java/com/example/hotelholidayv1/user/UserAdminServlet.java
@@ -6,33 +6,15 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebServlet({"/admin/store", "/admin/update", "/admin/get", "/admin/delete"})
+@WebServlet(name = "UserAdminServlet", value = "/UserAdminServlet")
 public class UserAdminServlet extends HttpServlet {
-    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Mapping(request, response);
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
-    public void Mapping(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
-        String path = request.getServletPath()!=null?request.getServletPath():"";
-        switch(path) {
-            case "/admin/get": getAllUserController(); break;
-            case "/admin/store": storeUserController(request); break;
-            case "/admin/update": updateUserController(); break;
-            case "/admin/delete": destroyUserController(); break;
-        }
-    }
-    public User convertToUserObject(HttpServletRequest request) {
-        return new User(request.getParameter("firstname"), request.getParameter("lastname"),
-                request.getParameter("email"), request.getParameter("password"));
-    }
-    public void getAllUserController() {
-    }
-    public void storeUserController(HttpServletRequest request) {
-        UserService.storeUserService(convertToUserObject(request));
-    }
-    public void updateUserController() {
-        UserService.updateUserService();
-    }
-    public void destroyUserController() {
-        UserService.deleteUserService();
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
 }
