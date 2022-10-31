8a45c13f5991c3bef6a8869de7c192869b8132be
idriss <idrissaithadou@gmail.com>
2022-10-31 13:28:31 +0100

    feat : some changes

--- src/main/java/com/example/hotelholidayv1/promotion/PromotionServlet.java
+++ src/main/java/com/example/hotelholidayv1/promotion/PromotionServlet.java
@@ -1,5 +1,6 @@
 package com.example.hotelholidayv1.promotion;
 
+import com.example.hotelholidayv1.helpers.DataConverter;
 import com.example.hotelholidayv1.room.Room;
 import com.google.gson.Gson;
 import jakarta.servlet.*;
@@ -9,13 +10,15 @@ import jakarta.servlet.annotation.*;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
-@WebServlet("/admin/promotion/update")
+@WebServlet({"/admin/promotion/update", "/admin/promotion/get"})
 @MultipartConfig
 public class PromotionServlet extends HttpServlet {
     public static final String IMAGES_FOLDER = "assets/uploads";
@@ -48,7 +51,7 @@ public class PromotionServlet extends HttpServlet {
     public void Mapping(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException, ParseException {
         String path = request.getServletPath()!=null?request.getServletPath():"";
         switch(path) {
-//            case "/admin/room/get": getAllRoomController(request,response); break;
+            case "/admin/promotion/get": getAllPromoController(response); break;
 //            case "/admin/room/one": getOneRoomController(request,response); break;
 //            case "/admin/room/store": storeRoomController(request,response); break;
             case "/admin/promotion/update": updatePromoController(request,response); break;
@@ -63,12 +66,22 @@ public class PromotionServlet extends HttpServlet {
     }
 
 
-
+    public void getAllPromoController(HttpServletResponse response) throws IOException, SQLException {
+        ResultSet promotions = PromotionService.getAllPromoService();
+        PrintWriter out = response.getWriter();
+        List<HashMap<String,Object>> rooms = DataConverter.toList(promotions);
+        String json = new Gson().toJson(rooms);
+        out.println(json);
+        out.flush();
+    }
     public void updatePromoController(HttpServletRequest request, HttpServletResponse response) throws ParseException, ServletException, IOException, SQLException {
         PrintWriter out = response.getWriter();
         Gson gson = new Gson();
         Integer[] roomsId = gson.fromJson(request.getParameter("roomsParam"), Integer[].class);
-        Part file = request.getPart("fileSrc");
+        Part file = null;
+        if(request.getParameter("statusFile").equals("true")){
+            file = request.getPart("fileSrc");
+        }
         String newName = PromotionService.addPromotionservice(request, roomsId, file, uploadPath);
         HashMap<String, String> isResponse = new HashMap<>();
         isResponse.put("src",newName);
--- src/main/java/com/example/hotelholidayv1/room/RoomService.java
+++ src/main/java/com/example/hotelholidayv1/room/RoomService.java
@@ -1,11 +1,18 @@
 package com.example.hotelholidayv1.room;
 
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.Part;
+
 import java.io.File;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 public class RoomService {
     public static ResultSet getAllRoomService(int id) {
@@ -22,18 +29,94 @@ public class RoomService {
     public static ResultSet getAllImageService(int idRoom){
         return RoomRepository.allImages(idRoom);
     }
-    public static void storeRoomService(Room room, List<String> images) throws SQLException {
+    public static void storeRoomService(HttpServletRequest request, Room room,String uploadPath) throws SQLException {
+        List<String> images = new ArrayList<>();
+        try {
+            long unixTime;
+            for ( Part part : request.getParts()) {
+                String fileName = getFileName( part );
+                System.out.println(fileName);
+                if(!fileName.equals("Default.file") && fileName != null) {
+                    unixTime = System.currentTimeMillis() / 1000L;
+                    String fullPath = uploadPath + File.separator + fileName;
+                    String extension = fileName.substring(fileName.lastIndexOf('.') + 1);
+                    String name = fileName.substring(0,fileName.lastIndexOf('.') - 1);
+                    String newName = name + unixTime + "." + extension;
+                    String newFileName = uploadPath + File.separator + newName;
+                    part.write(fullPath);
+                    File file1 = new File(fullPath);
+                    File file2 = new File(newFileName);
+                    file1.renameTo(file2);
+                    images.add(newName);
+                }
+            }
+        } catch (ServletException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        images.add(request.getParameter("images"));
         RoomRepository.save(room,images);
     }
-    public static boolean updateRoomService(Room room){
-        return RoomRepository.update(room);
+
+    /*
+     * Récupération du nom du fichier dans la requête.
+     */
+    public static String getFileName( Part part ) {
+        for ( String content : part.getHeader( "content-disposition" ).split( ";" ) ) {
+            if ( content.trim().startsWith( "filename" ) )
+                return content.substring( content.indexOf( "=" ) + 2, content.length() - 1 );
+        }
+        return "Default.file";
     }
-    public static boolean deleteRoomService(int id) throws SQLException {
+
+    public static boolean updateRoomService(Room room,HttpServletRequest request, String uploadPath) throws SQLException {
+        List<String> images = new ArrayList<>();
+        try {
+            long unixTime;
+            for ( Part part : request.getParts()) {
+                String fileName = getFileName( part );
+                System.out.println(fileName);
+                if(!fileName.equals("Default.file") && fileName != null) {
+                    unixTime = System.currentTimeMillis() / 1000L;
+                    String fullPath = uploadPath + File.separator + fileName;
+                    String extension = fileName.substring(fileName.lastIndexOf('.') + 1);
+                    String name = fileName.substring(0,fileName.lastIndexOf('.') - 1);
+                    String newName = name + unixTime + "." + extension;
+                    String newFileName = uploadPath + File.separator + newName;
+                    part.write(fullPath);
+                    File file1 = new File(fullPath);
+                    File file2 = new File(newFileName);
+                    file1.renameTo(file2);
+                    images.add(newName);
+                }
+            }
+        } catch (ServletException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        images.add(request.getParameter("images"));
+        RoomRepository.addImages(room.id_room, images);
+        RoomRepository.update(room);
+        return true;
+    }
+    public static boolean deleteRoomService(int id, String uploadPath) throws SQLException {
         ResultSet imagesOnRoom = RoomRepository.allImages(id);
         while (imagesOnRoom.next()) {
-            File file = new File(imagesOnRoom.getString("src"));
+            File file = new File(uploadPath + File.separator + imagesOnRoom.getString("src"));
             if(file.exists()) { file.delete(); }
         }
         return RoomRepository.delete(id);
+    }
+
+    public static boolean deleteRoomImageService(HttpServletRequest request, String uploadPath) throws SQLException {
+        ResultSet imagesOnRoom = RoomRepository.allImagesWithId(Integer.parseInt(request.getParameter("id")));
+        while (imagesOnRoom.next()) {
+            File file = new File(uploadPath + File.separator + imagesOnRoom.getString("src"));
+            if(file.exists()) { file.delete(); }
+        }
+         RoomRepository.deleteImages(Integer.parseInt(request.getParameter("id")));
+        return true;
     }
 }
